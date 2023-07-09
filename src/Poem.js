ec4b36ae12dc7cf2e4f25924a9ec4db69c38b266
CJ <christopher.galliart@gmail.com>
2023-07-08 20:09:37 -0500

    CloudFront Update

--- src/Poem.js
+++ src/Poem.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import './App.css';
 import DOMPurify from 'dompurify';
-import {useEffect, useState} from 'react';
 
 export default function Poem({ poemTitle, poem, changeAuthor, author }) {
-    const [trimmedAuthor, setTrimmedAuthor] = useState('');
-    useEffect(() => {
-        if(!author){
-            var el = document.createElement('html');
-            el.innerHTML = author;
-            setTrimmedAuthor(el.getElementsByTagName( 'a' )[0].innerText.trim());
-        }
-    }, [author]);
-  
-    if (!author) {
-      return null; 
-    }
+    
   
     return (
       <div>
         <div className="PoemTitle" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(poemTitle) }} />
-        <button className="AuthorButton" onClick={() => changeAuthor({trimmedAuthor})}>
-          <div className="Author" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(author) }} />
+        <button className="AuthorButton" onClick={() => changeAuthor({author})}>
+          by {author}
         </button>
         <div className="Author" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(poem) }} />
       </div>
